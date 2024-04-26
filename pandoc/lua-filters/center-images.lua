f8819147e3bcd5e412373a4826b8312968fc1178
Xavier Góngora <ixbalanque@protonmail.ch>
2024-04-26 17:35:05 -0600

    para centrar imágenes por defecto

---
+++ pandoc/lua-filters/center-images.lua
@@ -0,0 +1,20 @@
+-- Filter images with this function if the target format is LaTeX.
+if FORMAT:match 'latex' then
+  function Image (elem)
+    -- Surround all images with image-centering raw LaTeX.
+    return {
+      pandoc.RawInline('latex', '\\hfill\\break{\\centering'),
+      elem,
+      pandoc.RawInline('latex', '\\par}')
+    }
+  end
+end
+
+-- Filter images with this function if the target format is HTML
+if FORMAT:match 'html' then
+  function Image (elem)
+    -- Use CSS style to center image
+    elem.attributes.style = 'margin:auto; display: block;'
+    return elem
+  end
+end
